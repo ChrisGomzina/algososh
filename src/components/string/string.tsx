aec81724718ff05bc9b1349696f08c85091dbe91
Christina Gomzina <chrisgoncharova@yandex.ru>
2023-04-25 21:41:58 +0300

    Init

---
+++ src/components/string/string.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+
+export const StringComponent: React.FC = () => {
+  return (
+    <SolutionLayout title="Строка">
+    </SolutionLayout>
+  );
+};
