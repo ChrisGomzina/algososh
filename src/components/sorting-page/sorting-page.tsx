222e71f0c8ca926d696e62177a8b2a61500181e2
Christina Gomzina <chrisgoncharova@yandex.ru>
2023-05-03 20:33:34 +0300

    Adds markup of sorting-page

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,9 +1,38 @@
 import React from "react";
+import styles from "./sorting-page.module.css";
+
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { RadioInput } from "../ui/radio-input/radio-input";
+import { Button } from "../ui/button/button";
+import { Column } from "../ui/column/column";
+import { Direction } from "../../types/direction";
 
 export const SortingPage: React.FC = () => {
   return (
     <SolutionLayout title="Сортировка массива">
+      <form className={styles.form}>
+        <RadioInput
+          label="Выбор"
+        />
+        <RadioInput 
+          extraClass="ml-20"
+          label="Пузырёк"
+        />
+        <Button
+          extraClass="ml-30 mr-6"
+          text="По возрастанию"
+          sorting={Direction.Ascending}
+        />
+        <Button
+          extraClass="mr-40"
+          text="По убыванию"
+          sorting={Direction.Descending}
+        />
+        <Button
+          extraClass={styles.button}
+          text="Новый массив"
+        />
+      </form>
 
     </SolutionLayout>
   );
