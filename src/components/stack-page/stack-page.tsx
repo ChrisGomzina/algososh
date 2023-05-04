0709c293ef633efb32df8bd47960e9bff2a33d4d
Christina Gomzina <chrisgoncharova@yandex.ru>
2023-05-04 21:03:54 +0300

    Adds markup of stack-page

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,10 +1,36 @@
 import React from "react";
+import styles from "./stack-page.module.css";
+
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Button } from "../ui/button/button";
+import { Input } from "../ui/input/input";
+import { Circle } from "../ui/circle/circle";
+import { ElementStates } from "../../types/element-states";
 
 export const StackPage: React.FC = () => {
   return (
     <SolutionLayout title="Стек">
+      <form className={styles.form}>
+        <Input 
+          extraClass={styles.input}
+          type="text"
+          isLimitText={true}
+          maxLength={4}
+        />
+        <Button 
+          text="Добавить"
+        />
+        <Button 
+          text="Удалить"
+        />
+        <Button 
+          extraClass={styles.button_type_reset}
+          text="Очистить"
+        />
+      </form>
+      <div>
 
+      </div>
     </SolutionLayout>
   );
 };
