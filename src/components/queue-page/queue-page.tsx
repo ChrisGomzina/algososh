e418a735ed8d4eb6fb28a76fdb6729ba2057e175
Christina Gomzina <chrisgoncharova@yandex.ru>
2023-05-06 21:19:48 +0300

    Adds markup of queue-page

--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -1,10 +1,35 @@
 import React from "react";
+import styles from "./queue-page.module.css";
+
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Button } from "../ui/button/button";
+import { Input } from "../ui/input/input";
+import { Circle } from "../ui/circle/circle";
 
 export const QueuePage: React.FC = () => {
   return (
     <SolutionLayout title="Очередь">
+      <form className={styles.form}>
+        <Input 
+          extraClass={styles.input}
+          type="text"
+          isLimitText={true}
+          maxLength={4}
+        />
+        <Button 
+          text="Добавить"
+        />
+        <Button 
+          text="Удалить"
+        />
+        <Button 
+          extraClass={styles.button_type_reset}
+          text="Очистить"
+        />
+      </form>
+      <div className={styles.list}>
 
+      </div>
     </SolutionLayout>
   );
 };
